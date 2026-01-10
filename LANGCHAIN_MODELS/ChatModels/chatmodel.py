cce7c3c0ec772da39e630f19b6f016005e815107
AmmarJamil23 <ammarjamil7000@gmail.com>
2026-01-10 13:37:30 +0500

    old codes

--- LANGCHAIN_MODELS/ChatModels/chatmodel.py
+++ LANGCHAIN_MODELS/ChatModels/chatmodel.py
@@ -2,9 +2,10 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
+# tempperature is parameter that is used to control determinism of the output text
 
-model = ChatOpenAI(model='gpt-4')
+model = ChatOpenAI(model='gpt-4', temperature=1.5)
 
-result = model.invoke("WHat is the largest country by agriculture")
+result = model.invoke("Write lyrics of sing about me i am dying of thirst by kendrick lamar ")
 
 print(result.content)
---
+++ LANGCHAIN_MODELS/ChatModels/hf_api.py
@@ -0,0 +1,13 @@
+from langchain_huggingface import HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    max_new_tokens=128
+)
+
+response = llm.invoke("What is the capital of Hungary?")
+print(response)
